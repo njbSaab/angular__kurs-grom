6d9fd448ed6b9fef8571b01c8e8d87a80891b85f
Pro <Admin@Pro.local>
2023-02-06 14:43:16 +0200

    lerning created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCoomponent } from './user.coomponent';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCoomponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: 'product.component.html',
+  styleUrls: ['product.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class ProductComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+    console.log(1);
+  }
+}
